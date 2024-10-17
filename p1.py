24f7f2489105321a43ae8dafa6e64c9d61e24f7e
yi qxiaobai <1186144279@qq.com>
2024-10-17 23:12:35 +0800

    提交信息

--- p1.py
+++ p1.py
@@ -4,8 +4,16 @@ Desc: This is Code Desc
 """
 import pickle
 
-with open("data/home_pages.pkl", "rb") as file:
-    data = pickle.load(file)
+# with open("data/home_pages.pkl", "rb") as file:
+#     data = pickle.load(file)
+#
+#     print(data)
 
-    print(data)
+
+str1 = "hello world"
+print(str1.endswith("d"))
+print(str1.endswith("ld"))
+print(str1.endswith("lo"))
+print(str1.endswith("lo", 1, 5))
+print(str1.endswith(("d", "lo")))
 
